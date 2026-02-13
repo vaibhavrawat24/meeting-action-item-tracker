fccb46e60477b41bd04b0d179f2f173ebbc99791
Vaibhav <rawatvaibhav42@gmail.com>
2026-02-13 19:43:04 +0530

    fixed render crash due to db issue

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "dev-secret")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tracker.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///tracker.db"
+)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
